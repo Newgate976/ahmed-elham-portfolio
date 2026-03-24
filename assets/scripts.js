db6cd2342bab630cf29b1cb8730537ca60744623
Ahmed Elhad <elham.ahmed.sidi@gmail.com>
2026-03-24 18:23:30 +0300

    light mode update

--- assets/scripts.js
+++ assets/scripts.js
@@ -24,4 +24,17 @@ menuBtn.addEventListener("click", () => {
 
 "Développeur Full Stack..."
 
+// LightMode
+const btn = document.getElementById("theme-toggle");
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    btn.textContent = "🌙";
+  } else {
+    btn.textContent = "☀️";
+  }
+});
+
 localStorage
